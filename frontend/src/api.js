ec9bf9287705cb13e28a2915bb8a1eedaf0e05bb
Niral Ajmera <102373223+niral261@users.noreply.github.com>
2025-06-20 11:26:39 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,2 +1,2 @@
-export const API_URL = 'https://intervue-backend-azure.vercel.app/poll';
-export const SOCKET_URL = 'https://intervue-backend-azure.vercel.app';
+export const API_URL = 'https://intervue-backend-mu.vercel.app/poll';
+export const SOCKET_URL = 'https://intervue-backend-mu.vercel.app';
